ae3a22f982eb87f570941e04235d81e7718cface
radistoubalidis <rtoumpalidis@gmail.com>
2023-11-12 13:12:36 +0200

    load data script

--- load.py
+++ load.py
@@ -1,11 +1,11 @@
-import subprocess
+import argparse
+from ast import arg
 import sys
+import numpy as np
 import pandas as pd
 import json
-
 from tqdm import tqdm
-from modules import GithubClient, HuggingFaceClient
-import esprima
+from modules import HuggingFaceClient
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Text
@@ -17,11 +17,11 @@ Database Stuff Start
 """
 
 env = load_dotenv(dotenv_path='.env')
-db_username = env["db_username"]
-db_password = env["db_password"]
-db_host = env["db_host"]
-db_port = env["db_port"]
-db_name = env["db_name"]
+db_username = os.getenv("db_username")
+db_password = os.getenv("db_password")
+db_host = os.getenv("db_host")
+db_port = os.getenv("db_port")
+db_name = os.getenv("db_name")
 connection_string = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 engine = create_engine(connection_string)
 
@@ -36,6 +36,7 @@ def session_add(row):
 
 Base = declarative_base()
 
+# Dataset ORM
 class Dataset(Base):
     __tablename__ = 'datasets'
 
@@ -64,6 +65,7 @@ def store_dataset(eslint_out):
     eslint_df['sample_id'] = [x.split('/')[-1].split('.')[0][1:] for x in eslint_df['filePath']]
     
     table_rows = []
+    rows_inserted = 0
     for idx, row in eslint_df.iterrows():
         for msg in row['messages']:
             row_dict = {
@@ -96,7 +98,9 @@ def store_dataset(eslint_out):
             line=row['line'],
         )
         session_add(new_ds)
+        rows_inserted += 1
     session_commit()
+    return rows_inserted
 """
 Database Stuff End
 """
@@ -134,6 +138,19 @@ def writeJS(path, content):
     except Exception as e:
         raise e
 
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size')
+    parser.add_argument('--dataset_length')
+    parser.add_argument('--dataset_offset')
+    parser.add_argument('--samples_dir')
+    parser.add_argument('--eslint_outputs_dir')
+    args = parser.parse_args()
+    return args
+
+def test():
+    print(DS_LENGTH, DS_LENGTH, BATCH_SIZE)
+
 """
 Helper functions End
 """    
@@ -142,35 +159,45 @@ Helper functions End
 """
 Main
 """
-DS_LENGTH = 1000
-BATCH_SIZE = 100
 def main():
     print('Starting to Load Hugging Face Data..')
     hfc = HuggingFaceClient(HUGGINGFACE_TOKEN)
-    dataset_index = 0
-    for i in tqdm(range(dataset_index, DS_LENGTH)):
-        params = {
-            "dataset":"code_search_net",
-            "config":"javascript",
-            "split":"train",
-            'offset': dataset_index,
-            'length': BATCH_SIZE
-        }
-        res_ds = hfc.get('rows', params=params)
-        if res_ds.status_code != 200:
-            raise Exception(f"Hugging Face client error:\n{res_ds.json()}")
-        dataset = res_ds.json()
+    dataset_index = DS_OFFSET
+    rows_added = 0
+    pbar = tqdm(np.arange(dataset_index, DS_LENGTH))
+    for i in pbar:
+        # params = {
+        #     "dataset":"code_search_net",
+        #     "config":"javascript",
+        #     "split":"train",
+        #     'offset': dataset_index,
+        #     'length': BATCH_SIZE
+        # }
+        # res_ds = hfc.get('rows', params=params)
+        # if res_ds.status_code != 200:
+        #     raise Exception(f"Hugging Face client error:\n{res_ds.json()}")
+        # dataset = res_ds.json()
+        with open('dataset_batch.json','r') as f:
+            dataset = json.load(f)
         table_rows = getRows(dataset)
         for row in table_rows:
-            writeJS(f"samples/_{row['dataset_id']}.js", row['func_code_string'])
+            writeJS(f"{SAMPLES_DIR}/_{row['dataset_id']}.js", row['func_code_string'])
         
-        run_eslint(i)
-        if not os.path.exists(f"eslint_batch_{i}.json"):
-            raise Exception(f"Reading JS binaries failed.")
-        store_dataset(f"eslint_batch_{i}.json")
-        
+        pbar.set_postfix({'num_errors_warnings': len(table_rows)})
         dataset_index += BATCH_SIZE
+        
+    idx = 1
+    run_eslint(idx)
+    if not os.path.exists(f"{ESLINT_OUT_DIR}/eslint_batch_{idx}.json"):
+        raise Exception(f"Reading JS binaries failed.")
+    rows_added += store_dataset(f"{ESLINT_OUT_DIR}/eslint_batch_{idx}.json")
     
 if __name__ == '__main__':
+    args = getArgs()
+    BATCH_SIZE = int(args.batch_size)
+    DS_LENGTH = int(args.dataset_length)
+    DS_OFFSET = int(args.dataset_offset)
+    SAMPLES_DIR = args.samples_dir
+    ESLINT_OUT_DIR = args.eslint_outputs_dir
     main()
     
