b1feeb19a1ee049d03e430317bcf0b2351adcd7e
radistoubalidis <rtoumpalidis@gmail.com>
2023-11-12 13:11:54 +0200

    hugging face api client

---
+++ modules.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class HuggingFaceClient():
+    def __init__(self, token) -> None:
+        self.TOKEN = token
+        self.BASE_URL = "https://datasets-server.huggingface.co"
+        self.AUTH = {"Authorization":f"Bearer {self.TOKEN}"}
+        
+    def get(self, method, params=None):
+        if params != None:
+            return requests.get(
+                url=f"{self.BASE_URL}/{method}",
+                headers=self.AUTH,
+                params=params
+            )
+        else:
+            return requests.get(
+                url=f"{self.BASE_URL}/{method}",
+                headers=self.AUTH,
+            )
